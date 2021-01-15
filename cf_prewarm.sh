6dcc1250af5aab21bca22199a177b163521bf694
xiangqua <75667661+xiangqua@users.noreply.github.com>
2021-01-15 22:50:36 +0800

    Update cf_prewarm.sh

--- cf_prewarm.sh
+++ cf_prewarm.sh
@@ -5,6 +5,6 @@
 for file in cat file.txt
 do
 echo $file
-payload="{"filename":"$file","cloudfront_url":"d1zi40b7x5dwgb.cloudfront.net"}"
+payload="{\"filename\":\"$file\",\"cloudfront_url\":\"d1zi40b7x5dwgb.cloudfront.net\"}"
 aws lambda invoke --function-name cloudfront_prewarm --invocation-type Event --payload $payload o.out
 done
