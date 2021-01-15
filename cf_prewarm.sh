512781f9186aea72b26c1d91b21496c48e1e930f
xiangqua <75667661+xiangqua@users.noreply.github.com>
2021-01-15 21:47:41 +0800

    Create cf_prewarm.sh

---
+++ cf_prewarm.sh
@@ -0,0 +1,10 @@
+#Cloudfront url: d1zi40b7x5dwgb.cloudfront.net
+#Lambda function name: cloudfront_prewarm
+#Need aws cli
+
+for file in cat file.txt
+do
+echo $file
+payload="{"filename":"$file","cloudfront_url":"d1zi40b7x5dwgb.cloudfront.net"}"
+aws lambda invoke --function-name cloudfront_prewarm --invocation-type Event --payload $payload o.out
+done
